ab1959a373916e3ef9b058fd3207c14f4605f878
Frank <francho_714@hotmail.com>
2023-12-15 14:16:21 -0400

    we are done

--- assets/Script.js
+++ assets/Script.js
@@ -1,5 +1,26 @@
 
 axios.get("https://leonardoapi.onrender.com/music")
-.then((rec) => {
-     console.log(rec)
+.then((res) => {
+     res.data.map((song) => {
+          let ol = document.createElement('li')
+          ol.innerHTML = `${song.author} - ${song.title}`
+
+          document.querySelector("#tracklist").appendChild(ol)
+
+/*evento escuchando*/
+     
+          ol.addEventListener('click' , () => {
+               console.log(song.title)       
+
+               document.querySelector("#songtitle").innerHTML = song.title
+
+               document.querySelector("#author").innerHTML = song.author
+
+               document.querySelector("#songaudio").setAttribute ('src' , song.path.audio)
+
+               document.querySelector("#imageaudio").setAttribute ('src' , song.path.front)
+          })
+
+     })
 })
+
